4e6f38eb18d92ca35e744e1934cbb5f410a09c98
orenc <orenc@waves.com>
2022-04-19 11:17:24 +0300

    python 3.10

--- create_venv.sh
+++ create_venv.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-python3.9 -m venv venv
+python3.10 -m venv venv
 source venv/bin/activate
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install -r requirements_mac_only.txt
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r requirements.txt
+python3.10 -m pip install -r requirements_mac_only.txt
+
+#conda create --name installEnv
+#conda activate installEnv
+#conda install -c conda-forge PyYaml -y
